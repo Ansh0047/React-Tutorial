1db48671351fc8fcd0630eacda598ca408de9c3e
Ansh <anshkant090@gmail.com>
2024-08-05 00:50:22 +0530

    useState in class based componnets

---
+++ 09 class based components/src/components/Users.js
@@ -0,0 +1,77 @@
+import { Component } from 'react';
+import User from './User';
+
+import classes from './Users.module.css';
+
+// const DUMMY_USERS = [
+//   { id: 'u1', name: 'Max' },
+//   { id: 'u2', name: 'Manuel' },
+//   { id: 'u3', name: 'Julie' },
+// ];
+
+// class component
+class Users extends Component{
+  // it is basically used for initiallisation
+  constructor(){
+    super();
+    this.state = {
+      showUsers: true
+    };
+  }
+  // so, in this class based component we have to declare the function outside the render method and we can call this using this keyword
+  toggleUsersHandler(){
+    // this.state.showUsers = false; not use that
+    this.setState((curState) => {
+      return {showUsers: !curState.showUsers};
+    });
+  }
+
+  render(){
+    const usersList = (
+      <ul>
+        {/* to use the props inside the class components we have to use this.props.{prop name} */}
+        {this.props.users.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+
+// functional component
+// const Users = () => {
+//   const [showUsers, setShowUsers] = useState(true);
+
+//   const toggleUsersHandler = () => {
+//     setShowUsers((curState) => !curState);
+//   };
+
+//   const usersList = (
+//     <ul>
+//       {DUMMY_USERS.map((user) => (
+//         <User key={user.id} name={user.name} />
+//       ))}
+//     </ul>
+//   );
+
+//   return (
+//     <div className={classes.users}>
+//       <button onClick={toggleUsersHandler}>
+//         {showUsers ? 'Hide' : 'Show'} Users
+//       </button>
+//       {showUsers && usersList}
+//     </div>
+//   );
+// };
+
+export default Users;
