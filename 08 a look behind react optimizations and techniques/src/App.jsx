9d13d3c6fbc7e707ecb559d598d7eba9292a9646
Ansh <anshkant090@gmail.com>
2024-08-04 10:25:00 +0530

    use of memo and component structuring to prevent re render

--- 08 a look behind react optimizations and techniques/src/App.jsx
+++ 08 a look behind react optimizations and techniques/src/App.jsx
@@ -3,31 +3,30 @@ import { useState } from 'react';
 import Counter from './components/Counter/Counter.jsx';
 import Header from './components/Header.jsx';
 import { log } from './log.js';
+import ConfigureCounter from './components/Counter/ConfigureCounter.jsx';
 
 function App() {
   log('<App /> rendered');
-
-  const [enteredNumber, setEnteredNumber] = useState(0);
   const [chosenCount, setChosenCount] = useState(0);
 
-  function handleChange(event) {
-    setEnteredNumber(+event.target.value);
-  }
-
-  function handleSetClick() {
-    setChosenCount(enteredNumber);
-    setEnteredNumber(0);
+  function handleSetCount(newCount){
+    setChosenCount(newCount);
   }
 
   return (
     <>
       <Header />
       <main>
+        {/* here on every keystoke the app component re-render and also the child components */}
+        {/* but down there the counter component will be re-executed unnecessarily 
+        so, to prevent this we will move this code to other component */}
+        {/* this code is moverd to ConfigureCounter         
         <section id="configure-counter">
           <h2>Set Counter</h2>
           <input type="number" onChange={handleChange} value={enteredNumber} />
           <button onClick={handleSetClick}>Set</button>
-        </section>
+        </section> */}
+        <ConfigureCounter onSet={handleSetCount} />
         <Counter initialCount={chosenCount} />
       </main>
     </>
