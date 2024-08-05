36ea27147807647b1e890ee1965c308c60a0323c
Ansh <anshkant090@gmail.com>
2024-08-06 00:56:29 +0530

    sending http requests(get)

---
+++ 10 sending http requests/src/components/AvailablePlaces.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import Places from "./Places.jsx";
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    // used to send http requests to some other server 
+    // we can use it fetch and send the data
+    fetch("http://localhost:3000/places")
+      .then((response) => {
+        return response.json();
+      })
+      .then((resData) => {
+        setAvailablePlaces(resData.places);
+      });
+  }, []);
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
